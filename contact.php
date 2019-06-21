6c5bc542bc9779d06d438187d5639cb25997fb59
Himanshu Dayma <himudayma@gmail.com>
2019-06-21 15:41:49 +0530

    contact form

--- contact.php
+++ contact.php
@@ -1,18 +1,13 @@
 <?php
-
-if(isset($_POST["submit"])) {
-    $name=$_POST["name"];
-    $subject=$_POST["subject"];
-    $mailFrom=$_POST["mail"];
-    $message=$_POST["message"];
-
-	$mailTo="himanshudayma@thelectureroom.in";
-	$headers = "From: ".$mailFrom;
-	$text = "Mail recieved from ".$name."\n".$message;
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($name, $subject, $text, $headers);
-    header("Location: index.html?mailsend");
+$to = 'maryjane@email.com';
+$subject = $_POST["subject"];
+$message = $_POST["message"]; 
+$from = $_POST["mail"];
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
 }
 ?>
