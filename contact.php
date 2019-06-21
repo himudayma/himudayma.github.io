4a3f1b19688b707e4d8bd069572181266d586595
Himanshu Dayma <himudayma@gmail.com>
2019-06-21 15:15:08 +0530

    contact form testing

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST["submit"])) {
+    $name=$_POST["name"];
+    $subject=$_POST["subject"];
+    $mailFrom=$_POST["mail"];
+    $message=$_POST["message"];
+
+	$mailTo="himanshudayma@thelectureroom.in";
+	$headers = "From: ".$mailFrom;
+	$text = "Mail recieved from ".$name."\n".$message;
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($name, $subject, $text, $headers);
+    header("Location: index.html?mailsend");
+}
